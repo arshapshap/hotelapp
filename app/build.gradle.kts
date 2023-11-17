4d1f6730ce75f4221fadb8c0cdaadd79bd89d92f
arshapshap <arshapshap@mail.ru>
2023-11-17 18:33:38 +0300

    Add NavigationComponent dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.constraintlayout)
     implementation(libs.koin)
+    implementation(libs.bundles.navigation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
