93cee9dc09188b4e7f08cfffc811d4ba79dc8fa6
arshapshap <arshapshap@mail.ru>
2023-11-20 16:26:23 +0300

    Fix recycler view

--- feature/booking/src/main/java/com/arshapshap/hotelapp/feature/booking/presentation/screen/booking/touristsrecyclerview/TouristsAdapter.kt
+++ feature/booking/src/main/java/com/arshapshap/hotelapp/feature/booking/presentation/screen/booking/touristsrecyclerview/TouristsAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.EditText
 import androidx.core.view.isGone
-import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doAfterTextChanged
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -29,89 +28,118 @@ internal class TouristsAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-        ViewHolder(getBinding(parent))
+        ViewHolder(
+            binding = getBinding(parent),
+            onClickExpand = onClickExpand,
+            onFieldChanged = onFieldChanged)
 
     override fun getItemCount(): Int = list.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.binding.run {
-        (list[position]).let { tourist ->
-            setHeader(tourist)
-            setContentExpanded(tourist.isExpanded)
-            setContent(tourist)
-        }
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.onBind(list[position])
 
     private fun getBinding(parent: ViewGroup): LayoutBookingTouristInfoBinding =
         LayoutBookingTouristInfoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
-    private fun LayoutBookingTouristInfoBinding.setHeader(tourist: EditableTourist) {
-        textViewTouristNumber.text = root.context.resources.getString(
-            R.string.tourist_number,
-            formatAsOrdinal(tourist.id + 1)
-        )
-        imageButtonExpandTouristInfo.setOnClickListener {
-            onClickExpand(tourist.id)
+    class ViewHolder(
+        private val binding: LayoutBookingTouristInfoBinding,
+        private val onClickExpand: (Int) -> Unit,
+        private val onFieldChanged: (Int, EditableTouristField, String) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        private var touristId: Int? = null
+
+        init {
+            setTextChangedListeners()
+        }
+
+        fun onBind(tourist: EditableTourist) {
+            touristId = tourist.id
+
+            setHeader(tourist)
+            setContent(tourist)
+            setContentExpanded(tourist.isExpanded)
+        }
+
+        private fun setHeader(tourist: EditableTourist) {
+            with(binding) {
+                textViewTouristNumber.text = root.context.resources.getString(
+                    R.string.tourist_number,
+                    formatAsOrdinal(tourist.id + 1)
+                )
+                imageButtonExpandTouristInfo.setOnClickListener {
+                    onClickExpand(tourist.id)
+                }
+            }
+        }
+
+        private fun setContentExpanded(isExpanded: Boolean) {
+            with(binding) {
+                imageButtonExpandTouristInfo.rotation = if (isExpanded) 270f else 90f
+                textInputName.isGone = !isExpanded
+                textInputSurname.isGone = !isExpanded
+                textInputBirthday.isGone = !isExpanded
+                textInputCitizenship.isGone = !isExpanded
+                textInputPassportNumber.isGone = !isExpanded
+                textInputPassportValidityPeriod.isGone = !isExpanded
+            }
+        }
+
+        private fun setContent(tourist: EditableTourist) {
+            with(binding) {
+                editTextName.setNewText(tourist.name)
+                editTextSurname.setNewText(tourist.surname)
+                editTextBirthday.setNewText(tourist.birthday?.toString() ?: "")
+                editTextCitizenship.setNewText(tourist.citizenship)
+                editTextPassportNumber.setNewText(tourist.passportNumber?.toString() ?: "")
+                editTextPassportValidityPeriod.setNewText(
+                    tourist.passportValidityPeriod?.toString() ?: ""
+                )
+            }
+        }
+
+        private fun LayoutBookingTouristInfoBinding.formatAsOrdinal(number: Int) =
+            root.context.resources.getStringArray(R.array.ordinal_number)[number - 1]
+
+        private fun EditText.setNewText(value: String) {
+            if (text.toString() != value)
+                setText(value)
+        }
+
+        private fun setTextChangedListeners() {
+            with(binding) {
+                editTextName.doAfterTextChanged {
+                    touristId?.let { id ->
+                        onFieldChanged(id, EditableTouristField.Name, it.toString())
+                    }
+                }
+                editTextSurname.doAfterTextChanged {
+                    touristId?.let { id ->
+                        onFieldChanged(id, EditableTouristField.Surname, it.toString())
+                    }
+                }
+                editTextBirthday.doAfterTextChanged {
+                    touristId?.let { id ->
+                        onFieldChanged(id, EditableTouristField.Birthday, it.toString())
+                    }
+                }
+                editTextCitizenship.doAfterTextChanged {
+                    touristId?.let { id ->
+                        onFieldChanged(id, EditableTouristField.Citizenship, it.toString())
+                    }
+                }
+                editTextPassportNumber.doAfterTextChanged {
+                    touristId?.let { id ->
+                        onFieldChanged(id, EditableTouristField.PassportNumber, it.toString())
+                    }
+                }
+                editTextPassportValidityPeriod.doAfterTextChanged {
+                    touristId?.let { id ->
+                        onFieldChanged(id, EditableTouristField.PassportValidityPeriod, it.toString())
+                    }
+                }
+            }
         }
     }
-
-    private fun LayoutBookingTouristInfoBinding.setContentExpanded(isExpanded: Boolean) {
-        imageButtonExpandTouristInfo.rotation = if (isExpanded) 270f else 90f
-        textInputName.isGone = !isExpanded
-        textInputSurname.isGone = !isExpanded
-        textInputBirthday.isGone = !isExpanded
-        textInputCitizenship.isGone = !isExpanded
-        textInputPassportNumber.isGone = !isExpanded
-        textInputPassportValidityPeriod.isGone = !isExpanded
-    }
-
-    private fun LayoutBookingTouristInfoBinding.setContent(tourist: EditableTourist) {
-        editTextName.configureTextInput(
-            tourist.id,
-            tourist.name,
-            EditableTouristField.Name)
-
-        editTextSurname.configureTextInput(
-            tourist.id,
-            tourist.surname,
-            EditableTouristField.Surname)
-
-        editTextBirthday.configureTextInput(
-            tourist.id,
-            tourist.birthday?.toString() ?: "",
-            EditableTouristField.Birthday
-        )
-
-        editTextCitizenship.configureTextInput(
-            tourist.id,
-            tourist.citizenship,
-            EditableTouristField.Citizenship
-        )
-
-        editTextPassportNumber.configureTextInput(
-            tourist.id,
-            tourist.passportNumber?.toString() ?: "",
-            EditableTouristField.PassportNumber
-        )
-
-        editTextPassportValidityPeriod.configureTextInput(
-            tourist.id,
-            tourist.passportValidityPeriod?.toString() ?: "",
-            EditableTouristField.PassportValidityPeriod
-        )
-    }
-
-    private fun LayoutBookingTouristInfoBinding.formatAsOrdinal(number: Int) =
-        root.context.resources.getStringArray(R.array.ordinal_number)[number - 1]
-
-    private fun EditText.configureTextInput(touristId: Int, value: String, touristField: EditableTouristField) {
-        if (text.toString() != value)
-            setText(value)
-        doAfterTextChanged {
-            onFieldChanged(touristId, touristField, it.toString())
-        }
-    }
-
-    class ViewHolder(val binding: LayoutBookingTouristInfoBinding) : RecyclerView.ViewHolder(binding.root)
 
     private class DiffCallback(
         private val oldList: List<EditableTourist>,
